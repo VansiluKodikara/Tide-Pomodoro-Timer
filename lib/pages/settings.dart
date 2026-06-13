53a0ffa178370a4db132a2a6c2c00656e788b22f
VansiluKodikara <vansilu08@gmail.com>
2026-06-13 22:17:13 +0530

    UI Updates & Settings added

---
+++ lib/pages/settings.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro_timer/pages/pomodoroTimer.dart';
+
+class settings extends StatefulWidget {
+  const settings({super.key});
+
+  @override
+  State<settings> createState() => _settingsState();
+}
+
+class _settingsState extends State<settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: (){
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => pomodoroTimer(),
+                ),
+            );
+          },
+            icon: Icon(
+              Icons.close_rounded,
+              color: Colors.white,
+              textDirection: TextDirection.ltr,
+              ),
+            ),
+            title: Text("Settings"),
+            titleTextStyle: TextStyle(
+              color: Colors.white,
+            ),
+            centerTitle: true,
+            backgroundColor: const Color(0xFF0A5BFF),
+          ),
+
+
+    );
+  }
+}
